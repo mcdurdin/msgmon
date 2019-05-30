3168e2c36a5a9c1f6bf8ef5398923aefc2e89275
Marc Durdin <marc@keyman.com>
2019-05-31 05:35:12 +1000

    Tweaks to build scripts to be path-agnostic; fix mc for all configurations

--- source/build.sh
+++ source/build.sh
@@ -30,8 +30,8 @@ die ( ) {
 
 # Environment. Update this as required for platform versions
 
-MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
-export BDS="C:\Program Files (x86)\Embarcadero\Studio\19.0"
+MSBUILD="C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+export BDS="C:\\Program Files (x86)\\Embarcadero\\Studio\\19.0"
 
 # Default parameters
 
@@ -78,10 +78,37 @@ fi
 if [ "$X64_BUILD" = true ]; then
     "$MSBUILD" //m //p:Configuration="$CONFIG" //p:PLATFORM="x64" || die "Failed to build x64 capture"
 fi
+popd
 
-cp msgmon.man ../../build/bin/$CONFIG/msgmon.man
+# Install latest msgmon.man, updating paths accordingly
+
+pushd ../build/bin/$CONFIG
+
+WARNACCESS=0
+MANFILESRCPATH=../../../source/capture/MsgMon.man
+MANFILEBINPATH=./MsgMon.source.man
+
+cmp --silent $MANFILESRCPATH $MANFILEBINPATH || {
+    # If the file has 
+    cp $MANFILESRCPATH $MANFILEBINPATH
+
+    MANFILEPATH=$(pwd | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
+    MANFILEPATHESC=$(echo "$MANFILEPATH" | sed -e 's/\\/\\\\/'g)
+    sed -e "s/msgmon.capture.x86.dll/$MANFILEPATHESC\\\\\\0/g" < $MANFILEBINPATH > ./MsgMon.man
+
+    # If we fail to install because of permissions, we don't kill the script
+    # we just warn at the end.
+    wevtutil um MsgMon.man || [ $? -eq 5 ] && WARNACCESS=1
+    [ $WARNACCESS -eq 0 ] && wevtutil im MsgMon.man || [ $? -eq 5 ] && WARNACCESS=1
+    if [[ WARNACCESS -eq 0 ]]; then
+        # Successfully installed so let's just record that success
+        cp $MANFILESRCPATH $MANFILEBINPATH
+    fi
+}
 
 popd
+
+# Build x86 client app
 
 pushd client
 
@@ -91,6 +118,8 @@ fi
 
 popd
 
+# Build x64 host app
+
 pushd x64host
 
 if [ "$X64_BUILD" = true ]; then
@@ -98,3 +127,16 @@ if [ "$X64_BUILD" = true ]; then
 fi
 
 popd
+
+if [ $WARNACCESS -ne 0 ]; then
+    echo ""
+    echo "==============================================================================="
+    echo "WARNING"
+    echo "-------"
+    echo "The manifest was not installed due to insufficient permissions. If you have not"
+    echo "already installed it, start an Administrative terminal and either run this"
+    echo "script again, or run: "
+    echo "  wevtutil um $MANFILEPATH\\MsgMon.man"
+    echo "  wevtutil im $MANFILEPATH\\MsgMon.man"
+    echo "==============================================================================="
+fi
